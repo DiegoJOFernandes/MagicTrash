10f9313c5d94f32c8bdd0097eabc71fc6b47d3c8
Diego Fernandes <diegofernandes62@hotmail.com>
2022-11-29 14:56:49 -0300

    update env

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-require('dotenv').config('../.env')
+// require('dotenv').config('../.env')
 
 const connection = new Sequelize(process.env.DATABASE, process.env.USER_DB, process.env.PASS_DB,{
   host: process.env.HOST_DB,
